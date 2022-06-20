efbbc630c690e85ae57b42641647b344006a3a1e
Antonio Jose <ajherreratabaco@gmail.com>
2022-06-20 12:15:49 +0200

    Added classNames package and some css

--- src/components/grid/Cell.js
+++ src/components/grid/Cell.js
@@ -1,13 +1,22 @@
+import classNames from 'classnames';
 import './grid.css';
 
 function Cell({ letter = '', actual = false }) {
+  const cellClass = classNames(
+    {
+      square__letter: true,
+      'square--invalid': letter && !letter.valid && !actual,
+      'square--position': letter && letter.valid && !letter.position && !actual,
+      'square--valid': letter && letter.valid && letter.position && !actual,
+    },
+  );
   return (
     <div className="square">
-      <p className="square__letter">
+      <div className={cellClass}>
         {
-        actual ? letter : letter.letter
-      }
-      </p>
+          actual ? letter : letter.letter
+        }
+      </div>
     </div>
   );
 }
