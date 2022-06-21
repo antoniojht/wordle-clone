6afa05076dc82e6eb17ea25c263796656c4788fa
Antonio Jose <ajherreratabaco@gmail.com>
2022-06-21 08:40:27 +0200

    added all possible words, and control of errors with modal

--- src/App.js
+++ src/App.js
@@ -10,24 +10,34 @@ import Grid from './components/grid/Grid';
 import Keyboard from './components/keyboard/Keyboard';
 
 import './app.css';
+import possibleWords from './helpers/possibleWords';
 
 function App() {
-  const wordle = useSelector((state) => state.game.wordle);
+  const { wordle, life, attemps } = useSelector((state) => state.game);
   const actual = useSelector((state) => state.actualWord);
   const [counterLetter, setCounterLetter] = useState(0);
+  const [error, setError] = useState('');
 
   const dispatch = useDispatch();
 
   // TODO: extract
   const handleKeyDown = (event) => {
+    setError('');
     if (event.key === 'Backspace') {
       dispatch(popActualWord());
       setCounterLetter(counterLetter - 1);
-    } else if (event.key === 'Enter') {
-      dispatch(addWord(actual));
-      dispatch(resetActualWord());
-      dispatch(addLetter());
-      setCounterLetter(() => 0);
+    } else if (event.key === 'Enter' && life < attemps.length) {
+      if (possibleWords.length < wordle.length) {
+        setError('La palabra es demasiado corta');
+      } else if (!possibleWords.includes(actual.join(''))) {
+        setError('La palabra no existe');
+      }
+      if (error.length === 0) {
+        dispatch(addWord(actual));
+        dispatch(resetActualWord());
+        dispatch(addLetter());
+        setCounterLetter(() => 0);
+      }
     } else if (/^[a-zA-Z]{1}$/.test(event.key) && counterLetter < wordle.length) {
       dispatch(actualWord(event.key));
       setCounterLetter((prev) => prev + 1);
@@ -48,6 +58,7 @@ function App() {
       <div className="container">
         <Navbar />
         <hr className="border" />
+        {error && <div className="modal">{error}</div>}
         <Grid />
       </div>
       <Keyboard />
