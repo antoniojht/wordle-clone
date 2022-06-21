ac08370f145796a51bf35a9ebe7157b5b497e55d
Antonio Jose <ajherreratabaco@gmail.com>
2022-06-21 09:48:33 +0200

    Extract handleKeyDown to customHook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import {
-  actualWord, popActualWord, addWord, resetActualWord, addLetter,
-} from './actions/wordle';
+import { useEffect } from 'react';
 
 import Navbar from './components/navbar/Navbar';
 import Grid from './components/grid/Grid';
@@ -11,48 +6,10 @@ import Keyboard from './components/keyboard/Keyboard';
 
 import './app.css';
 import possibleWords from './helpers/possibleWords';
+import useKeyDown from './hooks/useKeydown';
 
 function App() {
-  const {
-    wordle, life, attemps, isCorrect,
-  } = useSelector((state) => state.game);
-  const actual = useSelector((state) => state.actualWord);
-  const [counterLetter, setCounterLetter] = useState(0);
-  const [error, setError] = useState('');
-
-  const dispatch = useDispatch();
-
-  // TODO: extract
-  const handleKeyDown = (event) => {
-    if (isCorrect) {
-      event.preventDefault();
-      return;
-    }
-
-    setError('');
-
-    if (event.key === 'Backspace') {
-      dispatch(popActualWord());
-      setCounterLetter(counterLetter - 1);
-    } else if (event.key === 'Enter' && life < attemps.length) {
-      if (possibleWords.length < wordle.length) {
-        setError('La palabra es demasiado corta');
-      } else if (!possibleWords.includes(actual.join(''))) {
-        setError('La palabra no existe');
-      }
-      if (error.length === 0) {
-        dispatch(addWord(actual));
-        dispatch(resetActualWord());
-        dispatch(addLetter());
-        setCounterLetter(() => 0);
-      }
-    } else if (/^[a-zA-Z]{1}$/.test(event.key) && counterLetter < wordle.length) {
-      dispatch(actualWord(event.key));
-      setCounterLetter((prev) => prev + 1);
-    } else {
-      event.preventDefault();
-    }
-  };
+  const [error, handleKeyDown] = useKeyDown();
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
